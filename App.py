ad25b5c5292a9ea214911b5623702274c3d30be2
nikolaospapa3 <nikolaospapa3@gmail.com>
2023-12-19 19:29:18 +0200

    made App as an ngrok transmit client

--- App.py
+++ App.py
@@ -1,17 +1,113 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, jsonify
+from threading import Thread, Lock
+import socket
+import random
+import time
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
+
+## If you want to use Locks Ctrl+F     #LOCK#      and change appropriately the code blocks there
+
+#LOCK#lock = Lock()  # Create a lock object
+
+time_var = ''  # Global variable to store the time value
+
 @app.route('/')
 def home():
-    return 'Hello, Oceanos NTUA!'
+    return render_template('index.html')
+
+@app.route('/reload')
+def reload():
+    #LOCK#with lock:    
+    global time_var
+    print(time_var)
+    return jsonify({'time_var': time_var})  
+
 
 app.route('/demo')
 def telemetry_demo():
     render_template('')
 
+def send_messages(server_socket):
+    global time_var
+    while True:
+        # 8ewroume mia ka8ysterhsh, an den mas aresei thn allazoyme
+        time.sleep(0.5)
+        # diabazw to arxeio
+        data = pd.read_csv('demo/csv/data2023.csv', delimiter=';')
+        # ftiaxnw dianysma me tis times apo thn teleytaia seira tou arxeiou
+        
+        ######## Random Index for Demo
+
+        start = 0  # Replace with your desired start index
+        end = len(data) - 1  # Replace with your desired end index
+
+        #  random integer between start and end
+        random_i = random.randint(start, end)
+
+        #######
+
+        data1 = np.array(data.iloc[random_i].values)    #### CHANGE !!!!! random_i to -1 for real time last data
+
+        #######
+
+        #print(str(data1.size)+"---------------------------------------------------")
+        # bazw se ka8e metablhth thn antistoixh timh apo to panw dianusma se morfh str gia na emfanizetai sthn o8onh swsta
+        current_time = str(data1[0])
+        lat = str(data1[1])
+        longt = str(data1[2])
+        speed = str(data1[3])
+        miles = str(data1[4])
+        miles_lap = str(data1[5])
+        rtc = str(data1[6])
+        millis = str(data1[7])
+        rpm = str(data1[8])
+        input_voltage = str(data1[9])
+        motor_watt = str(data1[10])
+        motor_tempMosfet = str(data1[11])
+        motor_tempMotor = str(data1[12])
+        motor_current = str(data1[13])
+        battery_current = str(data1[14])
+        motor_dutyCycle = str(data1[15])
+        motor_error = str(data1[16])
+        rasp_temp = str(data1[17])
+        battery_ampere = str(data1[18])
+        battery_voltage = str(data1[19])
+        charge = str(data1[20])
+        battery_temperature = str(data1[21])
+        message = str({"current_time": current_time,"latitude": lat, "longitude": longt,"speed": speed, "miles": miles, "miles_lap": miles_lap, "rtc": rtc,"millis": millis,"rpm": rpm,"input_voltage": input_voltage,"motor_watt":motor_watt,"motor_tempMosfet": motor_tempMosfet,"motor_tempMotor": motor_tempMotor,"motor_current":motor_current,"battery_current":battery_current,"motor_dutyCycle":motor_dutyCycle,"motor_error":motor_error, "rasp_temp": rasp_temp, "battery_ampere": battery_ampere, "battery_voltage": battery_voltage, "charge": charge, "battery_temperature": battery_temperature})
+        #stelnoyme to mnm ston server
+        server_socket.sendall(message.encode())
+        ### Update global time
+        
+        #LOCK#with lock:
+        time_var = current_time
+            
+  
 if __name__ == '__main__':
+
+    #with app.app_context():
+    #    g.time_var = ''
+
+    # Connect to the server using the TCP tunnel URL
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.connect(('8.tcp.eu.ngrok.io', 20375))
+    print("Connected to the server.")
+
+    # Receive the welcome message from the server
+    data = server_socket.recv(1024)
+    print(data.decode())
+
+    # Start separate threads for sending and receiving messages
+    send_thread = Thread(target=send_messages, args=(server_socket,))
+    send_thread.start()
+    
+
+    ###  Run the Flask app for the web browser
     app.run(debug=True)
 
          
---
+++ dummy_server.py
@@ -0,0 +1,81 @@
+import socket
+from threading import Thread
+from pyngrok import ngrok
+import csv
+from datetime import datetime
+import ast
+import pandas as pd
+import json
+import os
+
+dir = "/Users/vassdel/Library/CloudStorage/OneDrive-ΕθνικόΜετσόβιοΠολυτεχνείο/Oceanos/Arduino/vesc_can_bus_arduino"
+
+# rename the serverdata and save it to another folder (history)
+try:
+    old_fn = 'serverdata.csv'
+    new_fn = 'serverdata_' + datetime.today().strftime("%Y-%m-%d") + '_' + datetime.now().strftime("%H-%M-%S") + '.csv'
+
+    # Use os.path.join for proper path construction
+    os.rename(os.path.join(dir, old_fn), os.path.join(dir + "/server history/", new_fn))
+except:
+    pass
+
+'''Για να τρέξουμε αρχικά τον σερβερ, τρεχουμε το αρχειο ngrok.exe και του δινουμε την εντολη ngrok tcp 8080 or ngrok tcp --region=eu --remote-addr=3.tcp.eu.ngrok.io:28669 8080
+Μετα βλεπουμε το Forwarding και αλλαζουμε στο αρχειο ngrok_TRANSMIT καταλληλα την εντολη server_socket.connect(('---.tcp.eu.ngrok.io',-----))
+(^Αυτο είναι γιατι εχω φρι λαισενς αλλιως ειναι σταθερά και δεν εχουμε θέμα)
+Μετα τρέχουμε το Final_server.py και τον ngrok_TRANSMIT.py τρέχει στο ρασμπερι
+Για να δουμε το visualisation τρεχουμε το main1.py'''
+
+def send_messages(client_socket):
+    while True:
+        # Get a message from the user and send it to the client
+        message = input("")
+        client_socket.sendall(message.encode())
+
+def receive_messages(client_socket):
+    while True:
+        # Receive a message from the client and print it
+        # print("hello")
+        data = client_socket.recv(1024)
+        my_str = data.decode('utf-8').replace("'", '"')
+        #print(len(my_str))
+        #my_str2=my_str.replace(" ",'"')
+        if len(my_str)<=600:
+            if len(my_str)>=150:
+                data2 = json.loads(my_str)
+                with open('serverdata.csv', 'a') as csv_file: #open test1 in append mode, keep appending to the csv
+                    csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                #ta dedomena gia ta headers
+                #na prosthesw sto data to time
+                    csv_writer.writerow(data2) #writes one row at a time sto test1.csv
+                    csv_file.close()
+                if not data:
+                    break
+                print(f"{data.decode()}")
+
+# Set up the socket to listen for incoming connections
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('localhost', 8080))
+server_socket.listen(1)
+# Test gia na fanei oti leitoyrgei h syndesh
+fieldnames =["current_time", "latitude", "longitude", "speed", "miles", "miles_lap", "rtc", "millis", "rpm", "input_voltage", "motor_watt", "motor_tempMosfet", "motor_tempMotor", "motor_current", "battery_current","motor_dutyCycle", "motor_error", "rasp_temp", "battery_ampere", "battery_voltage", "charge", "battery_temperature", "autonomy"]
+print(fieldnames)
+
+#writing sto csv in append mode
+with open('serverdata.csv', 'a') as csv_file:
+	csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+	csv_writer.writeheader()
+
+
+# Accept incoming connections and start receiving data
+while True:
+    client_socket, addr = server_socket.accept()
+    print(f"Connected by {addr}")
+    # Prepei na stal8ei ena mhnyma apo ton server gia na mporei meta na dextei mhnymata toy client giati exoume krathsei thn epikoinwnia 2 way (h apla prepei na eimaste eygenikoi symfwna me ton skoun)
+    client_socket.sendall("Welcome to the chat room!\n".encode())
+
+    # Start separate threads for sending and receiving messages
+    send_thread = Thread(target=send_messages, args=(client_socket,))
+    recv_thread = Thread(target=receive_messages, args=(client_socket,))
+    send_thread.start()
+    recv_thread.start()
