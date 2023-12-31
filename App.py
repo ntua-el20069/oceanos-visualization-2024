bb330b1f4366080c612436e43ceb5e0588d4832f
nikolaospapa3 <nikolaospapa3@gmail.com>
2023-12-31 14:02:07 +0200

    thread changes and bash command to host

--- App.py
+++ App.py
@@ -6,6 +6,7 @@ import random
 import time
 import pandas as pd
 import numpy as np
+import asyncio
 from requestsAPI.send import send_website
 from requestsAPI.readline import readline
 
@@ -20,7 +21,7 @@ fieldnames =["current_time", "latitude", "longitude", "speed", "miles", "miles_l
 csv_url = 'static/csv/serverdata_2023-12-17_16-11-34.csv'  ### CHANGE with the path of the CSV you want to visualize Live (Normal Mode)
 
 host_write_csv_path = 'hostdata.csv'
-host_read_csv_path = 'static/csv/hostdata.csv'
+host_read_csv_path = f'/home/oceanosntua/oceanos-visualization-2024/static/csv/{host_write_csv_path}'
 
 def send_messages(server_socket):
     global data_now
@@ -28,67 +29,34 @@ def send_messages(server_socket):
     while True:
         # 8ewroume mia ka8ysterhsh, an den mas aresei thn allazoyme
         time.sleep(0.5)        # CHANGE (delay of taking data from CSV)
-        # diabazw to arxeio
-        
-        
-        data = pd.read_csv(csv_url, delimiter=',')
-        # ftiaxnw dianysma me tis times apo thn teleytaia seira tou arxeiou
-        
+        data = pd.read_csv(csv_url, delimiter=',')  # diabazw to arxeio
         ######## Random Index for Demo
         counter = ( counter + 1 ) % len(data) #random.randint(start, end)
-        random_i = counter
-        #######
-
-        data1 = np.array(data.iloc[random_i].values)    #### CHANGE !!!!! random_i to -1 for real time last data
         
-        #######
+        #print(f"\nIndex is: ########################      ######################  {counter}\n")
+        # ftiaxnw dianysma me tis times apo thn teleytaia seira tou arxeiou
+        data1 = np.array(data.iloc[counter].values)    #### CHANGE !!!!! counter to -1 for real time last data
 
         #print(str(data1.size)+"---------------------------------------------------")
         # bazw se ka8e metablhth thn antistoixh timh apo to panw dianusma se morfh str gia na emfanizetai sthn o8onh swsta
-        ''' # bad code ...
-        current_time = str(data1[0])
-        lat = str(data1[1])
-        longt = str(data1[2])
-        speed = str(data1[3])
-        miles = str(data1[4])
-        miles_lap = str(data1[5])
-        rtc = str(data1[6])
-        millis = str(data1[7])
-        rpm = str(data1[8])
-        input_voltage = str(data1[9])
-        motor_watt = str(data1[10])
-        motor_tempMosfet = str(data1[11])
-        motor_tempMotor = str(data1[12])
-        motor_current = str(data1[13])
-        battery_current = str(data1[14])
-        motor_dutyCycle = str(data1[15])
-        motor_error = str(data1[16])
-        rasp_temp = str(data1[17])
-        battery_ampere = str(data1[18])
-        battery_voltage = str(data1[19])
-        charge = str(data1[20])
-        battery_temperature = str(data1[21])
-        
         ### Update global data_now
-        data_now = {"current_time": current_time,"latitude": lat, "longitude": longt,"speed": speed, "miles": miles, "miles_lap": miles_lap, "rtc": rtc,"millis": millis,"rpm": rpm,"input_voltage": input_voltage,"motor_watt":motor_watt,"motor_tempMosfet": motor_tempMosfet,"motor_tempMotor": motor_tempMotor,"motor_current":motor_current,"battery_current":battery_current,"motor_dutyCycle":motor_dutyCycle,"motor_error":motor_error, "rasp_temp": rasp_temp, "battery_ampere": battery_ampere, "battery_voltage": battery_voltage, "charge": charge, "battery_temperature": battery_temperature}
-
-        '''
-        
-         ### Update global data_now
         data_now = {label : str(x) for label, x in zip(fieldnames, data1)}        
-       
-        send_website(data_now, host_write_csv_path)
+
+        asyncio.run(send_website(data_now, host_write_csv_path))
+        #print(data_now)
 
         message = str(data_now)
+        
         #stelnoyme to mnm ston server
         server_socket.sendall(message.encode())
+        
         
 @app.route('/')  # basic web route
 def home():
     return render_template('telemetry.html') 
 
 @app.route('/reload') # reload data, update visualization (a JavaScript function fetches this JSON data every 1 sec)
-def reload():   
+def reload():  
     global data_now
     if mode == 'local':
         return jsonify(data_now)
@@ -103,6 +71,9 @@ def example():
 def roundSlider():
     return render_template('demo.html')
 
+def visualization():
+    app.run(host='0.0.0.0', use_reloader=False, debug=True)
+
 if __name__ == '__main__':
 
     # Connect to the server using the TCP tunnel URL
@@ -112,13 +83,17 @@ if __name__ == '__main__':
     # Receive the welcome message from the server
     data = server_socket.recv(1024)
     print(data.decode())
-
-    # Start separate threads for sending and receiving messages
-    send_thread = Thread(target=send_messages, args=(server_socket,))
-    send_thread.start()
     print("Connected to the server.")
+    # Start separate threads for sending and receiving messages
+    #send_thread = Thread(target=send_messages, args=(server_socket,))
+    #send_thread.start()
+    
+    # Start separate thread for visualization
+    web_thread = Thread(target=visualization, args=())
+    web_thread.start()
 
-    ###  Run the Flask app for the web browser
-    app.run(debug=True)
+    # Execute send_messages in main program
+    send_messages(server_socket)
+    
+    
 
-         
--- requestsAPI/send.py
+++ requestsAPI/send.py
@@ -2,31 +2,31 @@ import requests
 import json
 import os
 from requestsAPI.account import account_info
+import aiohttp
 
 username, token, path = account_info()
-id = 31689948
+id = 31692730
 
 
-def send_website(data_now: dict, csv_name: str):
+async def send_website(data_now: dict, csv_name: str):
+    separator = ','
+    csv_line = separator.join(list(data_now.values())).replace('nan','')
     python_script = f'''
-import os
-newline_character = os.linesep
-separator = ','
-data_now = {data_now}
-csv_line = separator.join(list(data_now.values())).replace('nan','')
 with open('{csv_name}','a',encoding='utf-8') as csvFile:
-    csvFile.write(csv_line)
-    csvFile.write(newline_character)
+    print("{csv_line}", file=csvFile)         
 '''
-    console_log = {'input' : f'python\n{python_script}\nexit()\n'}
+    bash_script = f'''echo "{csv_line}" >> "{csv_name}" '''
+    #console_log = {'input' : f'python\n{python_script}\nexit()\n'}
+    console_log = {'input' : f'{bash_script}\n'}
 
-    response = requests.post(
-        f'https://www.pythonanywhere.com/api/v0/user/{username}/consoles/{id}/send_input/',
-        headers={'Authorization': f'Token {token}'},
-        json=console_log   
-    )
+    async with aiohttp.ClientSession() as session:
+        response = requests.post(
+            f'https://www.pythonanywhere.com/api/v0/user/{username}/consoles/{id}/send_input/',
+            headers={'Authorization': f'Token {token}'},
+            json=console_log   
+        )
 
-    print('Request Response:')
-    response_text = response.content.decode('utf-8')
-    print(response_text)
+        print('Request Response:')
+        response_text = response.content.decode('utf-8')
+        print(response_text)
     
